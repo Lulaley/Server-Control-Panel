9485b05f395fe2570a5aab1a3a4eb1700519dc2a
Lulaley <remi.joulin@gmail.com>
2024-07-10 15:29:54 +0200

    d

--- static/js/mcLogs.js
+++ static/js/mcLogs.js
@@ -28,6 +28,7 @@ async function fetchMinecraftLogFiltered() {
     if (document.getElementById('folder-select').value === "") {
         return;
     }
+    // Retrieve the filter type from local storage or use a default
     const filterType = localStorage.getItem('selectedFilterType') || 'all';
     highlightSelectedButton(filterType);
 
@@ -50,17 +51,48 @@ async function fetchMinecraftLogFiltered() {
         const consoleElement = document.getElementById('console');
         consoleElement.innerHTML = ''; // Clear existing logs
 
+        // Split the logs string into individual log entries
         try {
             const logEntries = data.logs.split('\n').filter(entry => 
                 !entry.includes('Thread RCON Client ** started') && 
                 !entry.includes('Thread RCON Client ** shutting down')
             );
             logEntries.forEach(entry => {
-                // Votre code pour traiter chaque entrée de log
+                const span = document.createElement('span');
+                span.textContent = entry;
+
+                // Determine log level and apply CSS class
+                if (entry.includes('/ERROR')) {
+                    span.className = 'error-message';
+                } else if (entry.includes('/WARN')) {
+                    span.className = 'warning-message';
+                } else if (entry.includes('<') || entry.includes('[Rcon]')) {
+                    // This condition now also checks for entries containing [RCON]
+                    span.className = 'discussion-message';
+                } else if (entry.includes('/INFO')) {
+                    span.className = 'info-message';
+                } else {
+                    // Default class for logs without a clear level, or you can add more conditions for other levels
+                    span.className = 'any-message';
+                }
+
+                consoleElement.appendChild(span);
+                consoleElement.appendChild(document.createElement('br')); // New line
             });
         } catch (processingError) {
             console.error('Erreur lors du traitement des entrées de log:', processingError);
         }
+        // Optionally, update the online players list if your API also returns this information            
+        const playerList = document.getElementById('players-list');
+        playerList.innerHTML = ''; // Clear existing list
+        for (const player of data.online_players) {
+            const li = document.createElement('li');
+            li.textContent = player;
+            playerList.appendChild(li);
+        }
+
+        // Scroll to the bottom of the console
+        consoleElement.scrollTop = consoleElement.scrollHeight;
     } catch (fetchError) {
         console.error('Erreur lors de la récupération des logs:', fetchError);
     }
