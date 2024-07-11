100b4ac41baa127846f6591003b2f35f3f987b98
Chimea <remi.joulin@gmail.com>
2024-07-11 22:48:02 +0200

    f

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-init_rcon_port(app)
+init_rcon_port()
 
 from routes.service import get_services, init_start_service_routes, init_stop_service_routes, init_restart_service_routes, init_delete_service_routes, init_create_service_routes
 from routes.logs import init_get_logs_routes
