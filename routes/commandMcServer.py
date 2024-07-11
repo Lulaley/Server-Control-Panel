a2a3d8dfb34377bf0d2237062f4b3cb54c314bf4
Chimea <remi.joulin@gmail.com>
2024-07-11 23:09:31 +0200

    f

--- routes/commandMcServer.py
+++ routes/commandMcServer.py
@@ -5,7 +5,7 @@ from .conf import MC_RCON_HOST, MC_RCON_PASSWORD, init_rcon_port, get_rcon_port
 def init_send_command(app):
     @app.route('/send_command', methods=['POST'])
     def send_command():
-        init_rcon_port(app)
+        init_rcon_port()
         command = request.form.get('command')
         try:
             with Client(MC_RCON_HOST, get_rcon_port(), passwd=MC_RCON_PASSWORD) as client:
