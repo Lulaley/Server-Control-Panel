d04e3006faca774f7f73d3bf1037d2ffb580c0f3
Chimea <remi.joulin@gmail.com>
2024-07-27 23:56:40 +0200

    f

--- routes/logs.py
+++ routes/logs.py
@@ -91,12 +91,16 @@ def init_get_logs_routes(app):
                             if timestamp_match:
                                 timestamp_str = timestamp_match.group(1)
                                 # Convert the timestamp to the desired format if necessary
-                                if len(timestamp_str) > 8:  # Format is [27Jul2024 23:28:48.567]
-                                    timestamp = datetime.strptime(timestamp_str, '%d%b%Y %H:%M:%S.%f')
-                                    formatted_timestamp = timestamp.strftime('%d-%m-%Y %H:%M:%S')
-                                else:  # Format is [23:40:48]
-                                    formatted_timestamp = timestamp_str
-                                line = f'[{formatted_timestamp}] {line[match.start():]}'
+                                try:
+                                    if len(timestamp_str) > 8:  # Format is like [27Jul2024 23:28:48.567]
+                                        timestamp = datetime.strptime(timestamp_str, '%d%b%Y %H:%M:%S.%f')
+                                        formatted_timestamp = timestamp.strftime('%d-%m-%Y %H:%M:%S')
+                                    else:  # Format is [23:40:48]
+                                        formatted_timestamp = timestamp_str
+                                    line = f'[{formatted_timestamp}] {line[match.start():]}'
+                                except ValueError:
+                                    # Handle the case where the timestamp format is incorrect
+                                    pass
                         filtered_lines.append(line)
                     elif filter_type == 'info' and '/INFO]' in line:
                         filtered_lines.append(line)
