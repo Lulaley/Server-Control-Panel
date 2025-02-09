01219d1485cf3bfe178500a0086259d22676cc29
Chimea <remi.joulin@gmail.com>
2025-02-09 18:26:29 +0100

    add restart button

--- routes/update.py
+++ routes/update.py
@@ -1,13 +1,22 @@
 import subprocess
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 update_bp = Blueprint('update', __name__)
 
-@update_bp.route('/api/update_server', methods=['POST'])
-def update_server():
+@update_bp.route('/api/aaction_server', methods=['POST'])
+def aaction_server():
+    data = request.get_json()
+    action = data.get('action')
+
     try:
-        subprocess.run(['sudo', 'apt-get', 'update'], check=True)
-        subprocess.run(['sudo', 'apt-get', 'upgrade', '-y'], check=True)
+        if action == 'update':
+            subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+            subprocess.run(['sudo', 'apt-get', 'upgrade', '-y'], check=True)
+        elif action == 'restart':
+            subprocess.run(['sudo', 'reboot'], check=True)
+        else:
+            return jsonify(success=False, error='Invalid action'), 400
+
         return jsonify(success=True)
     except subprocess.CalledProcessError as e:
         return jsonify(success=False, error=str(e))
